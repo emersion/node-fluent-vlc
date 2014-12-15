40a5504e778903e6dece6f134f422e3493a494a7
Emersion <contact@emersion.fr>
2014-12-15 20:14:54 +0100

    Adds options to video/audio codecs, adds stderr support

--- index.js
+++ index.js
@@ -26,12 +26,14 @@ VlcCommand.prototype.format = function (format) {
 	this._outputFormat = format;
 	return this;
 };
-VlcCommand.prototype.videoCodec = function (codec) {
+VlcCommand.prototype.videoCodec = function (codec, opts) {
 	this._outputVideoCodec = codec;
+	this._outputVideoOpts = opts;
 	return this;
 };
-VlcCommand.prototype.audioCodec = function (codec) {
+VlcCommand.prototype.audioCodec = function (codec, opts) {
 	this._outputAudioCodec = codec;
+	this._outputAudioOpts = opts;
 	return this;
 };
 
@@ -53,6 +55,8 @@ VlcCommand.prototype.addOption = function () {
 
 VlcCommand.prototype._spawn = function (args, options) {
 	var that = this;
+	
+	console.log('Starting VLC...', args);
 
 	var command = VlcCommand.getVlcPath();
 	var proc = spawn(command, args, options);
@@ -64,9 +68,14 @@ VlcCommand.prototype._spawn = function (args, options) {
 	if (typeof this._output != 'string') {
 		proc.stdout.pipe(this._output);
 	}
-console.log(args);
+
 	proc.on('exit', function () {
 		that.emit('exit');
+	});
+
+	proc.stderr.setEncoding('utf-8');
+	proc.stderr.on('data', function (data) {
+		process.stderr.write(data); // TODO: stderr handler
 	});
 };
 VlcCommand.prototype._getArguments = function () {
@@ -82,9 +91,19 @@ VlcCommand.prototype._getArguments = function () {
 	var transcode = [];
 	if (this._outputVideoCodec) {
 		transcode.push('vcodec='+this._outputVideoCodec);
+		if (this._outputVideoOpts) {
+			for (var name in this._outputVideoOpts) {
+				transcode.push(name+'='+this._outputVideoOpts[name]);
+			}
+		}
 	}
 	if (this._outputAudioCodec) {
 		transcode.push('acodec='+this._outputAudioCodec);
+		if (this._outputAudioOpts) {
+			for (var name in this._outputAudioOpts) {
+				transcode.push(name+'='+this._outputAudioOpts[name]);
+			}
+		}
 	}
 	if (transcode.length) {
 		outputOpts.push('transcode{'+transcode.join(',')+'}');
